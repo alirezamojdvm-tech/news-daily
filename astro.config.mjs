555fb798a5d6f098fe20c31abfa9242566efb4ac
Alireza Mohammadi Jafari <alireza.mo.j.dvm@gmail.com>
2026-07-30 17:15:04 +0330

    🔧 افزودن Sitemap و Robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://blog.hsc-co.ir', // آدرس سایت شما
+  integrations: [sitemap()],
+  // اگر می‌خواهید صفحات خاصی از نقشه سایت حذف شوند:
+  // sitemap: {
+  //   filter: (page) => page !== 'https://blog.hsc-co.ir/admin/'
+  // }
+});
