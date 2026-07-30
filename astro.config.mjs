5c5d569715feda57e2b53094fcab5f1fcb34544d
Alireza Mohammadi Jafari <alireza.mo.j.dvm@gmail.com>
2026-07-30 17:24:10 +0330

    🔧 فعال‌سازی Sitemap در Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,6 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://blog.hsc-co.ir', // آدرس سایت شما
+  site: 'https://blog.hsc-co.ir',
   integrations: [sitemap()],
-  // اگر می‌خواهید صفحات خاصی از نقشه سایت حذف شوند:
-  // sitemap: {
-  //   filter: (page) => page !== 'https://blog.hsc-co.ir/admin/'
-  // }
 });
